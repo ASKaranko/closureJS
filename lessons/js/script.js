163b3b2f446ee92bb9a82691d689c3a8e0321b14
Karanko Andrei <andr.karanko@gmail.com>
2020-10-15 23:00:09 +0300

    Усложненное домашнее задание по уроку номер 6 Замыкания

--- lessons/js/script.js
+++ lessons/js/script.js
@@ -1,33 +1,59 @@
 'use strict';
 
+  function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+  }
 
   function Number() {
 
-    const n = 50;
+    let n = getRandomIntInclusive(1, 100),
+      attempts = 10;
+
+      console.log(n);
 
     function guessNumber () {
 
-      let guess = prompt("Угадай число от 1 до 100");
-  
-      if (guess === null) {
-        alert("Игра окончена");
-      } else if (isNaN(guess) || (guess.trim() === '')) {
-        alert("Введите число");
-        guessNumber();
-      } else if (parseFloat(guess) > n) {
-          alert("Загаданное число меньше");
+      if (attempts > 0) {
+
+        let guess = prompt("Угадай число от 1 до 100");
+    
+        if (guess === null) {
+          alert("Игра окончена");
+        } else if (isNaN(guess) || (guess.trim() === '')) {
+          alert("Введите число");
           guessNumber();
-      } else if (parseFloat(guess) < n) {
-          alert("Загаданное число больше");
-          guessNumber();
-      } else if (parseFloat(guess) === n) {
-        alert("Поздравляю, Вы угадали!!!");
+        } else if (parseFloat(guess) > n) {
+            attempts--;
+            alert("Загаданное число меньше, осталось " + attempts + " попыток");
+            guessNumber();
+        } else if (parseFloat(guess) < n) {
+            attempts--;
+            alert("Загаданное число больше, осталось " + attempts + " попыток");
+            guessNumber();
+        } else if (parseFloat(guess) === n) {
+          let answer2 = confirm("Поздравляю, Вы угадали!!! Хотите сыграть еще?");
+          if (answer2 === true) {
+            Number();
+          } else {
+            alert("Игра окончена");
+          }
+        }
+
+      } else {
+        let answer = confirm("Попытки закончились, хотите сыграть еще?");
+        if (answer === true) {
+          Number();
+        } else {
+          alert("Игра окончена");
+        }
       }
 
     }
-    console.dir(guessNumber);
+
     guessNumber();
-    
+
   }
 
   Number();
